f9fe85ba09e6d20bee710f8303b50a01db0c450b
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2016-10-31 18:42:01 +0100

    Init

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,20 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		new App().proceed();
+	}
+
+	public void proceed() {
+		System.out.println("Hello World!");
+	}
+}
