f2f717f969a834700387c1ca9f4be0152afc6967
Olivier Cailloux <olivier.cailloux@gmail.com>
2020-04-03 20:11:36 +0200

    Added JUnit Test case

---
+++ src/main/resources/archetype-resources/src/test/java/MyTests.java
@@ -0,0 +1,18 @@
+package ${package};
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyTests {
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = LoggerFactory.getLogger(MyTests.class);
+
+	@Test
+	void testSomething() throws Exception {
+		LOGGER.info("Started tests.");
+		assertTrue(false);
+	}
+}
